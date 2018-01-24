887bf350e8b6fa965b6b06f6d7a13b9922c069d0
mkatrantzis <mkatrantzis@hotmail.com>
2018-01-25 00:34:30 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,5 @@
 # name: Discourse ALLOWALL
-# about: Sets X-Frame-Options to ALLOWALL 
 # version: 1
-# authors: riking, dconjar, musketyr
+# authors: mkatrantzis
 
 Rails.application.config.action_dispatch.default_headers.merge!({'Content-Security-Policy' => "frame-ancestors http://skipsochallenges.com.localhost"})
