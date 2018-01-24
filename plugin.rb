361bea2446eb4bb21e6f044d164286c146c0e765
mkatrantzis <mkatrantzis@hotmail.com>
2018-01-24 17:56:12 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,4 @@
 # version: 1
 # authors: riking, dconjar, musketyr
 
-Rails.application.config.action_dispatch.default_headers.merge!({'Content-Security-Policy' => "frame-ancestors 'http://skipsochallenges.com.localhost/'"})
+Rails.application.config.action_dispatch.default_headers.merge!({'Content-Security-Policy' => "frame-ancestors 'http://skipsochallenges.com.localhost'"})
